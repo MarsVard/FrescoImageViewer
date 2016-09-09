cd1a22780bf44c0b8ee82f5e51f31633b54828d1
Mars Vardanyan <bugz.inc@gmail.com>
2016-09-09 16:33:32 +0200

    added new Interface to use as listener

---
+++ frescoimageviewer/src/main/java/com/stfalcon/frescoimageviewer/ImageViewerListener.java
@@ -0,0 +1,10 @@
+package com.stfalcon.frescoimageviewer;
+
+/**
+ * Created by Mars on 09/09/16.
+ */
+
+public interface ImageViewerListener {
+    void onImageViewDismissed();
+    void onImageViewShown();
+}
